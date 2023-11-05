703d1f1dc5e68b6605b789bba3b3209525dd48c4
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-05 09:04:49 -0300

    se agrega validacion del numero ingresadoen el ejer7

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,7 +1,9 @@
-let num=parseInt(prompt("Ingrese un numero hasta 50: "));
+let num = parseInt(prompt("Ingrese un numero hasta 50: "));
 
-for (let i = num; i >= 1; i--) {
+if (isNaN(num)) document.write("No ingreso numero");
+else if (num > 0 && num <= 50) {
+  for (let i = num; i >= 1; i--) {
     let cadena = i.toString();
     document.write(cadena.repeat(i) + "<br>");
   }
-  
+} else document.write("Debe ingresar numero hasta 50");
