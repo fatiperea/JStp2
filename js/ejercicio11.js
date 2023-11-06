c4b55daa57ed3a5d4ad1b62314f6222793be2fb2
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-06 00:45:47 -0300

    mofifico estructura de control y agrego validacion para la edad

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -1,16 +1,26 @@
-let nombre1= prompt("Ingrese el primer nombre: ");
-let edad1= prompt("Ingrese su edad: ");
+const nombre1 = prompt("Ingrese el primer nombre: ");
+const edad1 = parseInt(prompt("Ingrese edad: "));
 
-let nombre2= prompt("Ingrese el segundo nombre: ");
-let edad2= prompt("Ingrese su edad: ");
+const nombre2 = prompt("Ingrese el segundo nombre: ");
+const edad2 = parseInt(prompt("Ingrese edad: "));
 
-let nombre3= prompt("Ingrese el tercer nombre: ");
-let edad3= prompt("Ingrese su edad: ");
+const nombre3 = prompt("Ingrese el tercer nombre: ");
+const edad3 = parseInt(prompt("Ingrese edad: "));
 
-let mayor=Math.max(edad1,edad2,edad3);
+if (isNaN(edad1) || isNaN(edad2) || isNaN(edad3))
+  document.write("No se ingreso edad valida");
+else {
+  switch (Math.max(edad1, edad2, edad3)) {
+    case edad1:
+      document.write(nombre1 + " es mayor");
+      break;
 
-if(edad1===mayor) document.write(nombre1+" es mayor");
-else if (edad2===mayor) document.write(nombre2+" es mayor");
-else if (edad3===mayor) document.write(nombre3+" es mayor");
+    case edad2:
+      document.write(nombre2 + " es mayor");
+      break;
 
-//document.write(mayor);
+    case edad3:
+      document.write(nombre3 + " es mayor");
+      break;
+  }
+}
